e7547ffb1605197dbbd6d33d89fc43ae9e65c2ce
前田 <sloth19920731@gmail.com>
2021-06-20 15:50:54 +0900

    バージョン1

---
+++ src/main/java/com/example/GitTrainingSpring/GitTrainingSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.GitTrainingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrainingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrainingSpringApplication.class, args);
+		System.out.print("これはバージョン1です");
+	}
+
+}
---
+++ src/test/java/com/example/GitTrainingSpring/GitTrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitTrainingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
