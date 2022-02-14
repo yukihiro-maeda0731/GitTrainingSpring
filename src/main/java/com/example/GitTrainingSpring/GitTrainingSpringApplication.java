9ef02081d7ab9a40ab1c97fcd0576ab5df45130d
前田 <sloth19920731@gmail.com>
2022-02-14 23:02:50 +0900

    リセット

--- src/main/java/com/example/GitTrainingSpring/GitTrainingSpringApplication.java
+++ src/main/java/com/example/GitTrainingSpring/GitTrainingSpringApplication.java
@@ -8,12 +8,7 @@ public class GitTrainingSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTrainingSpringApplication.class, args);
-		System.out.print("これはバージョン2ですmaster");
-		int a = 0;
-		System.out.print("これはバージョン2の1です");
-		System.out.print("これはバージョン2の2です");
-		System.out.print("master2です");
-		System.out.print("master3です");
+		System.out.print("これはmasterです");
 	}
 
 }
